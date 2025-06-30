89eb8209210a1707d82263a160aa2bd298351384
dj-python <pdj911@naver.com>
2025-06-30 15:19:36 +0900

    6/30

--- main.py
+++ main.py
@@ -23,6 +23,9 @@ class MainPusher:
     cTemp = 0
 
     def __init__(self):
+        self.idxExecProcess_load = None
+        self.idxExecProcess_Unload = False
+        self.isExecProcess_load = False
         self.sysLed_pico = Pin(25, Pin.OUT)
 
         #region Init GPIO_OUT
@@ -43,14 +46,23 @@ class MainPusher:
         #end region
         self.init_gpioOut()
 
-        TCPClient.init(server_ip= '', server_port='')
+        ipaddress = '192.168.1.105'
+        portNumber = 8005
+        gateway = '192.168.1.1'
+        server_ip = '192.168.1.2'
+        server_port = 8000
+
+        try:
+            TCPClient.init(ipAddress=ipaddress, portNumber=portNumber, server_ip= server_ip, server_port=server_port, gateway=gateway)
+        except Exception as e:
+            print(f"[-] Initialization Error: {str(e)}")
 
         self.rxMessage = str()
 
         self.cntExecProcess = 0
         self.cntTimeOutExecProcess = 0
 
-        self.isExecProcess_unit0p = False
+        self.isExecProcess_manualHandle = False
         self.idxExecProcess_unit0p = 0
 
         self.pusherStatus = PusherStatus.UNKNOWN
@@ -98,30 +110,37 @@ class MainPusher:
                 else:
                     self.replyMessage('Reset failed')
 
-            else:
+            elif self.rxMessage == 'Start':
                 if self.pusherStatus is PusherStatus.READY:
-                    # load/unload
-                    if self.rxMessage == 'Unload':
-                        self.idxExecProcess_loadUnload = 0
-                        self.isExecProcess_loadUnload = True
+                    self.idxExecProcess_load = 0
+                    self.isExecProcess_load = True
                     # unit operation
-                    else:
-                        self.idxExecProcess_unit0p = 0
-                        self.isExecProcess_unit0p = True
+                else:
+                    self.idxExecProcess_unit0p = 0
+                    self.isExecProcess_manualHandle = True
+                self.cntTimeOutExecProcess = 0
+                self.pusherStatus = PusherStatus.DOING
 
-                    self.cntTimeOutExecProcess = 0
-                    self.pusherStatus = PusherStatus.DOING
+            elif self.rxMessage == 'Finish':
+                if self.pusherStatus is PusherStatus.READY:
+                    self.idxExecProcess_Unload = 0
+                    self.isExecProcess_Unload = True
+                self.cntTimeOutExecProcess = 0
+                self.pusherStatus = PusherStatus.DOING
 
 
     def func_25msec(self):
         if self.isExecProcess_initPusherPos:
             self.execProcess_setPusherPos()
 
+    # 검사 시작(load), 검사 종료(unload), 수동 제어
     def func_100msec(self):
-        if self.isExecProcess_loadUnload:
-            self.execProcess_loadUnload()
-        elif self.isExecProcess_unit0p:
-            self.execProcess_unit0p()
+        if self.isExecProcess_load:
+            self.execProcess_load()
+        elif self.isExecProcess_Unload:
+            self.execProcess_Unload()
+        elif self.isExecProcess_manualHandle:
+            self.execProcess_manualHandle()
 
     def execProcess_setPusherPos(self):
         if self.idxExecProcess_initPusherPos == 0:                          # Pusher up
@@ -141,7 +160,7 @@ class MainPusher:
                 self.idxExecProcess_initPusherPos += 1
         elif self.idxExecProcess_initPusherPos == 3:                        # Pusher Back 확인
             self.pusherError = PusherError.PUSHER_BACK
-            if self.gpioIn_pusherBack :
+            if self.gpioIn_PusherBack :
                 self.pusherError = PusherError.NONE
                 self.pusherStatus = PusherStatus.READY
                 self.isInitedPusher = True                                  # 초기화 완료 처리
@@ -157,90 +176,99 @@ class MainPusher:
             self.isInitedPusher = False
             self.replyMessage('S' + self.rxMessage[1:5] + errorCode)
 
-    def execProcess_loadUnload(self):
-        # unload (Pusher Back)
-        if self.rxMessage[3:5] == '00':
-            if self.idxExecProcess_loadUnload == 0:                     # Pusher 상승
-                self.set_gpioOut(self.gpioOut_pusherUp, True)
-                self.set_gpioOut(self.gpioOut_pusherDown, False)
-                self.idxExecProcess_loadUnload += 1
-            elif self.idxExecProcess_loadUnload == 1:                   # Pusher 상승 확인
-                self.pusherError = PusherError.PUSHER_UP
-                if self.gpioIn_PusherUp:
-                    self.cntTimeOutExecProcess = 0
-                    self.cntExecProcess = 0
-                    self.idxExecProcess_loadUnload += 1
-            elif self.idxExecProcess_loadUnload == 2:                   # 대기 500msec
-                self.cntExecProcess += 1
-                if self.cntExecProcess >= 5:
-                    self.cntTimeOutExecProcess = 0
-                    self.idxExecProcess_loadUnload += 1
-            elif self.idxExecProcess_loadUnload == 3:                   # Pusher 후진
-                self.set_gpioOut(self.gpioOut_pusherFront, False)
-                self.set_gpioOut(self.gpioOut_pusherBack, True)
+    def execProcess_load(self):
+        if self.idxExecProcess_load == 0:                     # Pusher 초기상태 확인
+            if self.gpioIn_PusherUp and self.gpioIn_PusherBack :
+                self.cntTimeOutExecProcess = 0
+                self.idxExecProcess_load += 1
+            else:
+                if not self.gpioIn_PusherUp:
+                    self.pusherError = PusherError.PUSHER_UP
+                elif not self.gpioIn_PusherBack:
+                    self.pusherError = PusherError.PUSHER_BACK
+        if self.idxExecProcess_load == 1:                     # Pusher 전진 동작
+            self.set_gpioOut(self.gpioOut_pusherFront, True)
+            self.set_gpioOut(self.gpioOut_pusherBack, False)
+            self.idxExecProcess_load += 1
+        elif self.idxExecProcess_load == 2:                   # Pusher 전진 확인
+            self.pusherError = PusherError.PUSHER_FRONT
+            if self.gpioIn_PusherFront:
+                self.cntTimeOutExecProcess = 0
                 self.cntExecProcess = 0
-                self.idxExecProcess_loadUnload += 1
-            elif self.idxExecProcess_loadUnload == 4:                   # Pusher 후진 확인
-                self.pusherError = PusherError.PUSHER_BACK
-                if self.gpioIn_pusherBack:
-                    self.idxExecProcess_loadUnload = False
-                    self.pusherStatus = PusherStatus.READY
-                    self.pusherError = PusherError.NONE
-                    self.replyMessage('S' + self.rxMessage[1:5] + '000')
-
-        # load
-        else :
-            if self.idxExecProcess_loadUnload == 0:                     # Pusher 초기상태 확인
-                if self.gpioIn_PusherUp and self.gpioIn_pusherBack :
-                    self.cntTimeOutExecProcess = 0
-                    self.idxExecProcess_loadUnload += 1
-                else:
-                    if not self.gpioIn_PusherUp:
-                        self.pusherError = PusherError.PUSHER_UP
-                    elif not self.gpioIn_pusherBack:
-                        self.pusherError = PusherError.PUSHER_BACK
-            if self.idxExecProcess_loadUnload == 1:                     # Pusher 전진 동작
-                self.set_gpioOut(self.gpioOut_pusherFront, True)
-                self.set_gpioOut(self.gpioOut_pusherBack, False)
-                self.idxExecProcess_loadUnload += 1
-            elif self.idxExecProcess_loadUnload == 2:                   # Pusher 전진 확인
-                self.pusherError = PusherError.PUSHER_FRONT
-                if self.gpioIn_PusherFront:
-                    self.cntTimeOutExecProcess = 0
-                    self.cntExecProcess = 0
-                    self.idxExecProcess_loadUnload += 1
-            elif self.idxExecProcess_loadUnload == 3:                   # delay 500ms 부여 구간
-                self.cntExecProcess += 1
-                if self.cntExecProcess >= 5:
-                    self.cntTimeOutExecProcess = 0
-                    self.idxExecProcess_loadUnload += 1
-            elif self.idxExecProcess_loadUnload ==4:                   # Pusher 하강 동작
-                self.set_gpioOut(self.gpioOut_pusherUp, False)
-                self.set_gpioOut(self.gpioOut_pusherDown, True)
-                self.cntExecProcess = 0
-                self.idxExecProcess_loadUnload += 1
-            elif self.idxExecProcess_loadUnload == 5:                   # Pusher 하강 확인
-                self.pusherError = PusherError.PUSHER_DOWN
-                if self.gpioIn_PusherDown:
-                    self.isExecProcess_loadUnload = False
-                    self.pusherStatus = PusherStatus.READY
-                    self.pusherError = PusherError.NONE
-                    self.replyMessage('S' + self.rxMessage[1:5] + '000')
+                self.idxExecProcess_load += 1
+        elif self.idxExecProcess_load == 3:                   # delay 500ms 부여 구간
+            self.cntExecProcess += 1
+            if self.cntExecProcess >= 5:
+                self.cntTimeOutExecProcess = 0
+                self.idxExecProcess_load += 1
+        elif self.idxExecProcess_load ==4:                   # Pusher 하강 동작
+            self.set_gpioOut(self.gpioOut_pusherUp, False)
+            self.set_gpioOut(self.gpioOut_pusherDown, True)
+            self.cntExecProcess = 0
+            self.idxExecProcess_load += 1
+        elif self.idxExecProcess_load == 5:                   # Pusher 하강 확인
+            self.pusherError = PusherError.PUSHER_DOWN
+            if self.gpioIn_PusherDown:
+                self.isExecProcess_load = False
+                self.pusherStatus = PusherStatus.READY
+                self.pusherError = PusherError.NONE
+                self.replyMessage('S' + self.rxMessage[1:5] + '000')
 
         self.cntTimeOutExecProcess += 1
         if self.cntTimeOutExecProcess >= 30:
             errorCode = self.checkErrorCode()
 
-            self.isExecProcess_loadUnload = False
+            self.isExecProcess_load = False
             self.pusherStatus = PusherStatus.ERROR
             self.pusherError = PusherError.LOAD_UNLOAD
             self.replyMessage('S' + self.rxMessage[1:5] + errorCode)
 
+    def execProcess_Unload(self):
+        # unload (Pusher Back)
+        if self.idxExecProcess_load == 0:                     # Pusher 상승
+            self.set_gpioOut(self.gpioOut_pusherUp, True)
+            self.set_gpioOut(self.gpioOut_pusherDown, False)
+            self.idxExecProcess_load += 1
+        elif self.idxExecProcess_load == 1:                   # Pusher 상승 확인
+            self.pusherError = PusherError.PUSHER_UP
+            if self.gpioIn_PusherUp:
+                self.cntTimeOutExecProcess = 0
+                self.cntExecProcess = 0
+                self.idxExecProcess_load += 1
+        elif self.idxExecProcess_load == 2:                   # 대기 500msec
+            self.cntExecProcess += 1
+            if self.cntExecProcess >= 5:
+                self.cntTimeOutExecProcess = 0
+                self.idxExecProcess_load += 1
+        elif self.idxExecProcess_load == 3:                   # Pusher 후진
+            self.set_gpioOut(self.gpioOut_pusherFront, False)
+            self.set_gpioOut(self.gpioOut_pusherBack, True)
+            self.cntExecProcess = 0
+            self.idxExecProcess_load += 1
+        elif self.idxExecProcess_load == 4:                   # Pusher 후진 확인
+            self.pusherError = PusherError.PUSHER_BACK
+            if self.gpioIn_PusherBack:
+                self.idxExecProcess_load = False
+                self.pusherStatus = PusherStatus.READY
+                self.pusherError = PusherError.NONE
+                self.replyMessage('S' + self.rxMessage[1:5] + '000')
+
+        self.cntTimeOutExecProcess += 1
+        if self.cntTimeOutExecProcess >= 30:
+            errorCode = self.checkErrorCode()
+
+            self.isExecProcess_Unload = False
+            self.pusherStatus = PusherStatus.ERROR
+            self.pusherError = PusherError.LOAD_UNLOAD
+            self.replyMessage('S' + self.rxMessage[1:5] + errorCode)
+
+
+
     def replyMessage(self, message):
-        if self.rxMessage[1:3] == '31':
+        if self.rxMessage == 'Check_status':
             pass
         else:
-            TCPClient.send_data(self.TCP_Server, message)
+            TCPClient.sendMessage(self.TCP_Server, message)
 
     def checkErrorCode(self):
         errorCode = '001'
@@ -259,7 +287,7 @@ class MainPusher:
             errorCode = '015'
         return errorCode
 
-    def execProcess_unit0p(self):
+    def execProcess_manualHandle(self):
         pass
 
 if __name__ == "__main__":
